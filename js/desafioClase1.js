40c91e140a214a57ed27e13177044065fd5dc9b9
Nicolas Corti <nicolasecorti@gmail.com>
2021-07-03 20:29:49 -0300

    Entrega desafio clase 1

---
+++ js/desafioClase1.js
@@ -0,0 +1,21 @@
+let numero1 = prompt("Ingrese un número");
+
+if(numero1 > 1000){
+    alert("El número ingrsado en mayor a 1000.");
+}
+
+
+
+let texto = prompt("Ingrese una palabra");
+
+if(texto == "Hola"){
+    console.log(texto, "es igual que Hola");
+}
+
+
+
+let numero2 = prompt("Ingrese otro número");
+
+if(numero2 > 10 && numero2 < 50){
+    alert(numero2 + " está entre 10 y 50.");
+}
